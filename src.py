42ffccd31987c866e0176d2f01cf396a77a87264
shivaborusu <shivaborusu@gmail.com>
2020-12-16 10:56:36 +0530

    first base files commit

---
+++ src.py
@@ -0,0 +1,34 @@
+## this must be implemented in OOP, I am thinking to create one class for student record and one class for hashtable
+
+
+#Design a hash function HashId() which accepts the applicant’s name as a parameter and returns the hash value
+def HashId(stname):
+    #to mimic the functionality intitally planning to with 
+    #python dictionaries slowly remove them later once the project is executing
+    pass
+
+#This function creates an empty hash table and points to null
+def initializeHash(self):
+    # I suppose the project must be planned in OOPS
+    pass
+
+#This function inserts the student’s name and corresponding details into the hash table
+def insertAppDetails(ApplicationRecords, name, phone, country, program, status): 
+    pass
+
+#This function finds the applicant’s details based on the name and updates the corresponding details into the hash table.
+def updateAppDetails(ApplicationRecords, name, phone, country, program, status):
+    pass
+
+#This function prints the list of all applicants who have applied to a particular program
+def memRef(ApplicationRecords, Program):
+    pass
+
+#This function prints the list of number of applications in their current stage 
+# - of the application process including Applied, Rejected and Approved
+def appStatus(ApplicationRecords):
+    pass
+
+#This function destroys all the entries inside hash table. This is a clean-up code.
+def destroyHash(ApplicationRecords):
+    pass
